e9aac9cde9da0f5b570f95c1d05ef4151b8f78bd
Leonardo <leosimoes.engcomp@gmail.com>
2024-04-01 11:32:23 -0300

    Project started

---
+++ src/main/java/com/app/jjwt/JavaSpringJjwtApplication.java
@@ -0,0 +1,13 @@
+package com.app.jjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringJjwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringJjwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/jjwt/JavaSpringJjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.jjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringJjwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
